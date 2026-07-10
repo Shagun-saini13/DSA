ffb5a42e22ea9e628e8bd5a93bf41c0a4dc5a133
Shagun-saini13 <sainishagun001@gmail.com>
2026-07-10 16:09:32 +0530

    added

---
+++ 2_If_Else_And_Loops/2_Condition_Package.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int package;
+    cout<<"Enter Package: ";
+    cin>>package;
+    if(package>10){
+        cout<<"Accepted";
+    }
+    else{
+        cout<<"Rejected";
+    }
+}
